16bc1830c7651f247f47a52f47b58c7d42444447
Lahlouh Nassim <lahlouhnassim@gmail.com>
2025-10-15 22:13:20 +0200

    register & login

---
+++ models/user.model.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose");
+
+const userSchema = mongoose.Schema({
+    _id: mongoose.Schema.Types.ObjectId,
+    nom: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true, // Empêche d'avoir 2 comptes avec le même email
+        lowercase: true, // Convertit l'email en minuscules
+        match: /[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$/ // Validation format email
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+module.exports = mongoose.model("User", userSchema);
--- routeur.js
+++ routeur.js
@@ -5,13 +5,149 @@ const livreSchema=require("./models/livres.modele");
 
 const mongoose = require("mongoose");
 
-
+const userSchema = require("./models/user.model");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
 routeur.get("/", (requete, reponse) => {
     reponse.render("accueil.html.twig")
 });  
 
 
+// Route pour afficher le formulaire d'inscription
+routeur.get("/register", (requete, reponse) => {
+    reponse.render("register.html.twig");
+});
+
+// Route pour traiter l'inscription
+routeur.post("/register", (requete, reponse) => {
+    // 1. Vérifier si l'email existe déjà
+    userSchema.findOne({ email: requete.body.email })
+        .then(utilisateurExistant => {
+            if (utilisateurExistant) {
+                // L'email existe déjà
+                requete.session.message = {
+                    type: 'danger',
+                    contenu: 'Cet email est déjà utilisé'
+                };
+                return reponse.redirect("/register");
+            }
+
+            // 2. Hasher le mot de passe
+            bcrypt.hash(requete.body.password, 10, (err, hash) => {
+                if (err) {
+                    console.log(err);
+                    return reponse.status(500).json({ error: err });
+                }
+
+                // 3. Créer le nouvel utilisateur
+                const user = new userSchema({
+                    _id: new mongoose.Types.ObjectId(),
+                    nom: requete.body.nom,
+                    email: requete.body.email,
+                    password: hash // On stocke le mot de passe hashé
+                });
+
+                // 4. Sauvegarder dans la base de données
+                user.save()
+                    .then(resultat => {
+                        console.log("Utilisateur créé:", resultat);
+                        requete.session.message = {
+                            type: 'success',
+                            contenu: 'Inscription réussie ! Vous pouvez maintenant vous connecter.'
+                        };
+                        reponse.redirect("/login");
+                    })
+                    .catch(error => {
+                        console.log(error);
+                        reponse.status(500).json({ error: error });
+                    });
+            });
+        })
+        .catch(error => {
+            console.log(error);
+            reponse.status(500).json({ error: error });
+        });
+});
+
+// Route pour afficher le formulaire de connexion
+routeur.get("/login", (requete, reponse) => {
+    reponse.render("login.html.twig");
+});
+
+// Route pour traiter la connexion
+routeur.post("/login", (requete, reponse) => {
+    // 1. Chercher l'utilisateur par email
+    userSchema.findOne({ email: requete.body.email })
+        .then(user => {
+            if (!user) {
+                // Aucun utilisateur trouvé avec cet email
+                requete.session.message = {
+                    type: 'danger',
+                    contenu: 'Email ou mot de passe incorrect'
+                };
+                return reponse.redirect("/login");
+            }
+
+            // 2. Comparer le mot de passe entré avec le hash en BDD
+            bcrypt.compare(requete.body.password, user.password, (err, result) => {
+                if (err) {
+                    requete.session.message = {
+                        type: 'danger',
+                        contenu: 'Erreur lors de la connexion'
+                    };
+                    return reponse.redirect("/login");
+                }
+
+                if (result) {
+                    // 3. Le mot de passe est correct ! Créer un token JWT
+                    const token = jwt.sign(
+                        {
+                            userId: user._id,
+                            email: user.email,
+                            nom: user.nom
+                        },
+                        "CLÉ_SECRÈTE_SUPER_SÉCURISÉE", // On changera ça plus tard
+                        {
+                            expiresIn: "24h" // Le token expire après 24h
+                        }
+                    );
+
+                    // 4. Stocker les infos de l'utilisateur dans la session
+                    requete.session.user = {
+                        id: user._id,
+                        nom: user.nom,
+                        email: user.email,
+                        token: token
+                    };
+
+                    requete.session.message = {
+                        type: 'success',
+                        contenu: 'Connexion réussie ! Bienvenue ' + user.nom
+                    };
+                    
+                    reponse.redirect("/livres");
+                } else {
+                    // Mot de passe incorrect
+                    requete.session.message = {
+                        type: 'danger',
+                        contenu: 'Email ou mot de passe incorrect'
+                    };
+                    reponse.redirect("/login");
+                }
+            });
+        })
+        .catch(error => {
+            console.log(error);
+            reponse.status(500).json({ error: error });
+        });
+});
+
+// Route pour se déconnecter
+routeur.get("/logout", (requete, reponse) => {
+    requete.session.destroy();
+    reponse.redirect("/");
+});
 
 routeur.get("/livres", (requete, reponse)=>{
 
